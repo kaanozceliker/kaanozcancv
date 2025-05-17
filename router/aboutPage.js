e56e8a2bf87f45b6029291d050368cbe44610edf
kaanozceliker <155054432+kaanozceliker@users.noreply.github.com>
2025-05-17 04:18:04 +0300

    Son Eklemeler Yapıldı Canlıya Hazır Hale getirildi

--- router/aboutPage.js
+++ router/aboutPage.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const data = {imgURL:'/img/background/about.jpg', title:'Why U.S.', subTitle:`We Are a Family`}
+const data = {
+  imgURL: '/img/background/about.jpg',
+  title: 'Why Me',
+  subTitle: 'What you need to know about me and why you should work with me'
+}
 
+router.get('/', (req, res) => {
+  res.render('site/about', {
+    headerData: data,
+    isHome: false
+  });
+});
 
-router.get('/', (req, res)=>{
-   res.render('site/about', {
-      headerData:data
-   })
-})
-
-
-
-module.exports = router
+module.exports = router;
--- router/mainPage.js
+++ router/mainPage.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const data = {imgURL:'/img/background/home.jpg', title:'Özcan Software', subTitle:`Work With Us`}
-const {join} = require('path');
+const data = {
+  imgURL: '/img/background/home.jpg',
+  title: 'Kaan Özcan',
+  subTitle: 'Real Estate-Management-Software'
+}
+const { join } = require('path');
 const Contents = require(join(__dirname, '..', 'model', 'content.js'))
 
+router.get('/', async (req, res) => {
+  let contents = await Contents.find();
+  contents = contents.reverse();
 
-router.get('/', async(req, res)=>{
-   let contents = await Contents.find();
-   contents = contents.reverse()
-   res.render('site/index', {
-      headerData:data,
-      contents: contents.map(item=>item.toJSON())
-   })
-})
+  res.render('site/index', {
+    headerData: data,
+    isHome: true,
+    contents: contents.map(item => item.toJSON())
+  });
+});
 
-
-module.exports = router
+module.exports = router;
